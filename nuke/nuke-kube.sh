5adff0cc6206802faa6ebda63cb486ef36699b4d
Sokoow <sokoow@gmail.com>
2018-11-04 11:27:22 +0000

    Playing with examples generator

--- nuke/nuke-kube.sh
+++ nuke/nuke-kube.sh
@@ -6,7 +6,7 @@ sudo kubeadm reset
 
 echo "Removing CI stack data and database storage"
 
-sudo rm -rf /home/$USER/.kube-volumes/postgresql
+sudo rm -rf /home/$USER/.kube-volumes/postgres
 sudo rm -rf /home/$USER/.kube-volumes/minio
 sudo rm -rf /home/$USER/.kube-volumes/gogs
 sudo rm -rf /home/$USER/.kube-volumes/drone
