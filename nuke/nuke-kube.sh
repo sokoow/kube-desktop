e5686596ce41cb68c2ca347183c9e3c57c198626
Sokoow <sokoow@gmail.com>
2019-11-18 17:51:35 +0100

    Cleanups

--- nuke/nuke-kube.sh
+++ nuke/nuke-kube.sh
@@ -6,9 +6,4 @@ sudo kubeadm reset
 
 echo "Removing CI stack data and database storage"
 
-sudo rm -rf /home/$USER/.kube-volumes/postgres
-sudo rm -rf /home/$USER/.kube-volumes/minio
-sudo rm -rf /home/$USER/.kube-volumes/gogs
-sudo rm -rf /home/$USER/.kube-volumes/drone
-sudo rm -rf /home/$USER/.kube-volumes/registry
 rm ../examples/.psql_initalized
