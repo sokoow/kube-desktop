6a249a964a50271e2c2955dbffe06f7f3ab578e6
Sokoow <sokoow@gmail.com>
2019-11-22 17:17:32 +0100

    Changing ssl generator for docker-registry

--- deploy-ci-stack.sh
+++ deploy-ci-stack.sh
@@ -18,9 +18,27 @@ echo "Installing docker-registry"
 pushd .
 cd charts/docker-registry/files
 
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -x509 -days 365 -out tls.crt -config openssl.conf -new -subj /C=EU
+# openssl genrsa -out rootCA.key 4096
+# openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.crt -extfile openssl2.conf -extensions req_ext
+# openssl req -new -sha256 \
+#     -key tls.key \
+#     -subj "/C=US/ST=CA/O=MyOrg, Inc./CN=registry-svc" \
+#     -reqexts SAN \
+#     -config <(cat /etc/ssl/openssl.cnf \
+#         <(printf "\n[SAN]\nsubjectAltName=DNS:registry-svc,DNS:registry.mykube.awesome")) \
+#     -out tls.csr
+# openssl x509 -req -in tls.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out tls.crt -days 500 -sha256 -extfile openssl2.conf -extensions req_ext
+#
+# openssl req -newkey rsa:4096 -nodes -sha256 -keyout ca.key -x509 -days 365 -config openssl.conf -out ca.crt -new -subj /C=EU
+# openssl req -newkey rsa:4096 -nodes -sha256 -CA ca.crt -CAkey ca.key -keyout tls.key -out tls.csr -config openssl.conf -new -subj /C=EU
+# openssl x509 -req -days 365 -in tls.csr -CA ca.crt -extfile openssl.conf -CAkey ca.key -CAcreateserial -out tls.crt
+#
+# openssl req -newkey rsa:4096 -nodes -sha256 -keyout tls.key -x509 -days 365 -out tls.crt -config openssl.conf -new -subj /C=EU
+
 mkdir -p /etc/docker/certs.d/registry-svc:5000
-cp ./tls.crt /etc/docker/certs.d/registry-svc:5000/ca.crt
+cp ./rootCA.crt /etc/docker/certs.d/registry-svc:5000/ca.crt
+cp registry-svc.key tls.key
+cp registry-svc.crt tls.crt
 kubectl create secret generic registry-tls-cert --from-file=./tls.key --from-file=./tls.crt
 
 popd
