4ba4ff042e831999133e3781af2a4706198c0254
Sokoow <sokoow@gmail.com>
2019-12-03 19:29:28 +0100

    Trying to fix ansible shenenigans

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,7 +6,7 @@ set -o pipefail
 apt-get update
 UCF_FORCE_CONFFNEW=1 DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::=--force-confnew mc git curl wget sudo make python
 
-echo "Opening curl"
+echo "Installing docker CE"
 wget https://get.docker.com -qO /tmp/get-docker.sh
 chmod +x /tmp/get-docker.sh
 /tmp/get-docker.sh
--- terraform-aws/scripts/userdata.sh
+++ terraform-aws/scripts/userdata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/authorized_keys
+mkdir -p /home/ubuntu/.kube
+ln -s /home/ubuntu/.kube /root/.kube
