78dad692229a93a30bfda8ceacd25cf53691c8b5
developer <developer@mykube.awesome>
2019-12-11 12:59:12 +0000

    Adding nextcloud prerereq shenenigans

---
+++ TODO/nextcloud-config/my.config.php
@@ -0,0 +1,15 @@
+<?php
+    $CONFIG = array (
+      'overwrite.cli.url' => 'https://next.mykube.awesome',
+      'overwritehost' => 'next.mykube.awesome:443',
+      'overwriteprotocol' => 'https',
+      'trusted_domains' =>
+       array (
+          0 => 'next.mykube.awesome',
+       ),
+      'trusted_proxies' =>
+      array (
+          0 => 'next.mykube.awesome',
+      ),
+    );
+
